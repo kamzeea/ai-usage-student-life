4a2ec6b08a1e639b0beb3923ac82ee13894903bb
elliefiedler24 <elliefiedler24@gmail.com>
2025-10-09 15:08:44 -0400

    Create SessionLength_and_TaskType_T-test.py

---
+++ SessionLength_and_TaskType_T-test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from scipy import stats
+
+df = pd.read_csv('updated_cleaned_dataset1.csv')
+
+groups = [group['SessionLengthMin'].values for name, group in df.groupby('TaskType')]
+
+f_stat, p_value = stats.f_oneway(*groups)
+
+print(f'F-statistic: {f_stat:.2f}')
+print(f'P-value: {p_value:.2f}')
